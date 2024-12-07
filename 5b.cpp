0c3150be10dbc6a8f72f19cd525e905ebe41ff33
Ruy Brito Barbosa <bruy@fb.com>
2024-12-07 14:22:49 +0000

    Add problem 5b

--- 5b.cpp
+++ 5b.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
@@ -10,12 +9,6 @@ bool container_contains(vector<pair<int, int> >& container, pair<int, int> v) {
     }
   }
   return false;
-  // vector<pair<int, int> >::iterator it = std::lower_bound(
-  //   container.begin(),
-  //   container.end(),
-  //   v
-  // );
-  // return it != container.end() && *it == v;
 }
 
 bool is_query_correct(vector<int> &query, vector<pair<int, int> >& input) {
@@ -29,17 +22,15 @@ bool is_query_correct(vector<int> &query, vector<pair<int, int> >& input) {
   return true;
 }
 
-void visit(vector<pair<int, int> >& input, vector<int> &query, vector<int> &ans, int mark[], int val) {
-  if (mark[val]) return;
-
-  mark[val] = 1;
-  for (int i = 0; i < input.size(); i++) {
-    if (input[i].second == val) {
-      visit(input, query, ans, mark, input[i].first);
+vector<int> bubble_sort(vector<int> &query, vector<pair<int, int> >& input) {
+  for (int i = 0; i < query.size(); i++) {
+    for (int j = i+1; j < query.size(); j++) {
+      if (container_contains(input, make_pair(query[j], query[i]))) {
+        swap(query[i], query[j]);
+      }
     }
   }
-
-  ans.push_back(val);
+  return query;
 }
 
 int main() {
@@ -69,44 +60,15 @@ int main() {
 
   int count = 0;
   for (int i = 0; i < queries.size(); i++) {
+    // Query is correct
     if (is_query_correct(queries[i], input)) {
       continue;
     }
 
-    // for (int j = 0; j < queries[i].size(); j++) {
-    //   cout << queries[i][j] << " ";
-    // }
-    // cout << endl;
-
-    vector<int> ans;
-    int mark[101] = {0};
-    for(int j = 0; j < queries[i].size(); j++) {
-      visit(input, queries[i], ans, mark, queries[i][j]);
-    }
-    
-    vector<int> filtered;
-    for(int j = 0; j < ans.size(); j++) {
-      for (int k = 0; k < queries[i].size(); k++) {
-        if (ans[j] == queries[i][k]) {
-          filtered.push_back(ans[j]);
-          break;
-        }
-      }
-    }
-
-    // for (int j = 0; j < filtered.size(); j++) {
-    //   cout << filtered[j] << " ";
-    // }
-    // cout << endl;
-    
-    count += filtered[filtered.size()/2];
+    vector<int> ans = bubble_sort(queries[i], input);
+    count += ans[ans.size()/2];
   }
 
   cout << count << endl;
   return 0;
 }
-
-
-// 5114
-// 5150
-// 4868
