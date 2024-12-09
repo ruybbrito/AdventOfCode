f376ecc27164b31fa872e3f2a1a6a455cd687143
Ruy Brito Barbosa <bruy@fb.com>
2024-12-09 18:27:29 +0000

    Adding 9b

--- 9b.cpp
+++ 9b.cpp
@@ -8,7 +8,7 @@ pair<int, int> findGap(vector<pair<int, int> >& gaps, int len, int r) {
   pair<int, int> bestGap = make_pair(-1, -1);
   for (int i = 0; i < gaps.size(); i++) {
     if (gaps[i].first < r && gaps[i].second >= len) {
-      if (bestGap.first < gaps[i].first) {
+      if (bestGap.first == -1 || bestGap.first > gaps[i].first) {
         bestGap = gaps[i];
       }
     }
@@ -17,19 +17,36 @@ pair<int, int> findGap(vector<pair<int, int> >& gaps, int len, int r) {
   return bestGap;
 }
 
+void removeGap(vector<pair<int, int> >& gaps, pair<int, int> old_gap) {
+  for (int i = 0; i < gaps.size(); i++) {
+    if (gaps[i] == old_gap) {
+      gaps.erase(gaps.begin() + i);
+      return;
+    }
+  }
+
+  return;
+}
+
 int main() {
   string s;
   cin >> s;
 
   string mem = "";
   map<int, long long int> id;
-  vector<pair<int, int> > gaps;
+  vector<pair<int, int> > gaps, blocks;
   for (int i = 0; i < 100 * s.size(); i++) id[i] = -1;
   
   int x = 1, cur_id = 0;
   for (int i = 0; i < s.size(); i++) {
+    // Found gap
     if (!x && s[i]-'0' > 0) {
       gaps.push_back(make_pair(mem.size(), s[i]-'0'));
+    }
+
+    // Found block
+    if (x && s[i]-'0' > 0) {
+      blocks.push_back(make_pair(mem.size(), s[i]-'0'));
     }
 
     for (int j = 0; j < s[i]-'0'; j++) {
@@ -44,40 +61,32 @@ int main() {
     if (x) cur_id++;
   }
 
-  // Fix the memory
-  // for (int a = 0, b = mem.size()-1; a < b;) {
-  //   if (mem[a] == '.' && mem[b] != '.') {
-  //     swap(mem[a], mem[b]);
-  //     id[a] = id[b];
-  //     id[b] = -1;
-  //     a++, b--;
-  //   } else if (mem[a] != '.') {
-  //     a++;
-  //   } else if (mem[b] == '.') {
-  //     b--;
-  //   }
-  // }
-  for (int i = mem.size() - 1; i >= 0; i--) {
-    if (mem[i] == '.') continue;
-    
-    int len = 1;
-    if (i > 0) {
-      for (int j = i-1; j >= 0; j--) {
-        if (mem[j] == mem[i]) len++;
-        else {
-          i = j+1;
-        };
-      }
+  reverse(blocks.begin(), blocks.end());
+
+  for (int i = 0; i < blocks.size(); i++) {
+    pair<int, int> gap = findGap(gaps, blocks[i].second, blocks[i].first);
+    if (gap.first == -1) {
+      // Can't find a placement for the block
+      continue;
     }
 
-    pair<int, int> gap = findGap(gaps, len, i);
-    // Fix memory using the gap
+    removeGap(gaps, gap);
+    if (blocks[i].second != gap.second) {
+      pair<int, int> newGap = make_pair(gap.first + gap.second - blocks[i].second + 1, gap.second - blocks[i].second);
+      gaps.push_back(newGap);
+    }
+
+    // Fix the memory
+    for (int j = 0; j < blocks[i].second; j++) {
+      swap(mem[gap.first + j], mem[blocks[i].first + j]);
+      id[gap.first + j] = id[blocks[i].first + j];
+      id[blocks[i].first + j] = -1;
+    }
   }
 
   long long int ans = 0;
   for (long long int i = 0; i < mem.size(); i++) {
-    if (mem[i] == '.') break;
-    ans += i * id[i];
+    if (mem[i] != '.') ans += i * id[i];
   }
   cout << ans << endl;
 
