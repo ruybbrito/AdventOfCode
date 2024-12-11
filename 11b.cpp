c21e9c768a348ea7b37592189384a39deae42ea9
Ruy Brito Barbosa <bruy@fb.com>
2024-12-11 10:28:24 +0000

    Add working 11b

--- 11b.cpp
+++ 11b.cpp
@@ -4,38 +4,43 @@
 
 using namespace std;
 
+long long int mem[1000000][76];
+
 long long int solve(long long int n, int cur) {
   if (cur == 0) return 1;
+  else if (n < 1000000 && mem[n][cur] != -1) return mem[n][cur];
 
-  if (!n) return solve(1, cur-1);
-  else if (n < 10) return solve(2024 * n, cur-1);
-  else if (n < 100) return solve(n/10, cur-1) + solve(n%10, cur-1);
-  else if (n < 1000) return solve(2024 * n, cur-1);
-  else if (n < 10000) return solve(n/100, cur-1) + solve(n%100, cur-1);
-  else if (n < 100000) return solve(2024 * n, cur-1);
-  else if (n < 1000000) return solve(n/1000, cur-1) + solve(n%1000, cur-1);
-  else if (n < 10000000) return solve(2024 * n, cur-1);
-  else if (n < 100000000) return solve(n/10000, cur-1) + solve(n%10000, cur-1);
-  else if (n < 1000000000) return solve(2024 * n, cur-1);
-  else if (n < 10000000000) return solve(n/100000, cur-1) + solve(n%100000, cur-1);
-  else if (n < 100000000000) return solve(2024 * n, cur-1);
-  else if (n < 1000000000000) return solve(n/1000000, cur-1) + solve(n%1000000, cur-1);
-  else if (n < 10000000000000) return solve(2024 * n, cur-1);
-  else if (n < 100000000000000) return solve(n/10000000, cur-1) + solve(n%10000000, cur-1);
+  long long int ans = -1;
+  
+  if (!n) ans = solve(1, cur-1);
   else {
-    cout << "ERROR" << endl;
-    return 0;
+    string s = to_string(n);
+    if (s.size() % 2 == 0) {
+      ans = solve(stoll(s.substr(0, s.size() / 2)), cur-1) + solve(stoll(s.substr(s.size() / 2, s.size() / 2)), cur-1);
+    } else {
+      ans = solve(2024*n, cur-1);
+    }
   }
+  
+  if (n < 1000000) mem[n][cur] = ans;
+  return ans;
 }
 
 int main() {
   vector<long long int> v;
-  long long int a, ans = 0;
+  long long int a;
   while (cin >> a) {
     v.push_back(a);
-    ans += solve(a, 75);
   }
 
+  for (int i =0; i < 1000001; i++) {
+    for (int j = 0; j < 76; j++) {
+      mem[i][j] = -1;
+    }
+  }
+
+  long long int ans = 0;
+  for (int i = 0; i < v.size(); i++) ans += solve(v[i], 75);
   cout << ans << endl;
 
   return 0;
