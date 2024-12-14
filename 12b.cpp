0490b1f467ba6856512538b79020df2c46328a3a
Ruy Brito Barbosa <bruy@fb.com>
2024-12-14 00:02:23 +0000

    Add working version of 12b

--- 12b.cpp
+++ 12b.cpp
@@ -13,10 +13,14 @@ bool is_oob(vector<string>& m, int i, int j) {
   return i < 0 || j < 0 || i >= m.size() || j >= m[0].size();
 }
 
-bool can_go(vector<string>& m, int i, int j, int dir) {
+bool cant_go(vector<string>& m, int i, int j, int dir) {
   int iNext = dir == 1 ? i+1 : dir == 3 ? i-1 : i;
   int jNext = dir == 0 ? j+1 : dir == 2 ? j-1 : j;
-  return !is_oob(m, iNext, jNext) && m[i][j] == m[iNext][jNext];
+  return is_oob(m, iNext, jNext) || m[i][j] != m[iNext][jNext];
+}
+
+bool can_go(vector<string>& m, int i, int j, int dir) {
+  return !cant_go(m, i, j, dir);
 }
 
 int find_area(vector<string>& m, int i, int j, int mark[200][200]) {
@@ -32,36 +36,36 @@ int find_area(vector<string>& m, int i, int j, int mark[200][200]) {
   return a;
 }
 
-int find_sides(vector<string>& m, int i, int j, int mark[200][200]) {
-  int dir = 0, initDir = 0;
-  int x = i, y = j;
-  int s = 0;
+int find_corners(vector<string>& m, int i, int j) {
+  int ans = 0;
+
+  // 90 degree turns
+  if (cant_go(m, i, j, 3) && cant_go(m, i, j, 2)) ans++;
+  if (cant_go(m, i, j, 1) && cant_go(m, i, j, 2)) ans++;
+  if (cant_go(m, i, j, 1) && cant_go(m, i, j, 0)) ans++;
+  if (cant_go(m, i, j, 3) && cant_go(m, i, j, 0)) ans++; 
   
-  while (true) {
-    mark[x][y] = 1;
-    
-    if (!can_go(m, x, y, dir)) {
-      s++;
-      dir = (dir + 1) % 4;
-    } else {
-      x = dir == 1 ? x+1 : dir == 3 ? x-1 : x;
-      y = dir == 0 ? y+1 : dir == 2 ? y-1 : y;
+  // 270 degree turns
+  if (can_go(m, i, j, 3) && can_go(m, i, j, 2) && m[i][j] != m[i-1][j-1]) ans++;
+  if (can_go(m, i, j, 1) && can_go(m, i, j, 2) && m[i][j] != m[i+1][j-1]) ans++;
+  if (can_go(m, i, j, 1) && can_go(m, i, j, 0) && m[i][j] != m[i+1][j+1]) ans++;
+  if (can_go(m, i, j, 3) && can_go(m, i, j, 0) && m[i][j] != m[i-1][j+1]) ans++;
 
-      // Check if we should change direction
-      int dirPrev = (((dir - 1) % 4) + 4) % 4;
-      if (can_go(m, x, y, dirPrev)) {
-        dir = dirPrev;
-        s++;
-      }
-    }
+  return ans;
+}
 
-    if (x == i && y == j && dir == initDir) break;
-  }
+int aggregate_corners(vector<string>& m, map<pair<int, int>, int>& corners, int mark[200][200], int i, int j) {
+  if (mark[i][j]) return 0;
 
-  // Find if there are islands of non- m[i][j]
-  // If yes, then check their sides and add to s
-  
-  return s;
+  mark[i][j] = 1;
+  int c = corners[make_pair(i, j)];
+
+  if (!is_oob(m, i+1, j) && !mark[i+1][j] && m[i][j] == m[i+1][j]) c += aggregate_corners(m, corners, mark, i+1, j);
+  if (!is_oob(m, i-1, j) && !mark[i-1][j] && m[i][j] == m[i-1][j]) c += aggregate_corners(m, corners, mark, i-1, j);
+  if (!is_oob(m, i, j+1) && !mark[i][j+1] && m[i][j] == m[i][j+1]) c += aggregate_corners(m, corners, mark, i, j+1);
+  if (!is_oob(m, i, j-1) && !mark[i][j-1] && m[i][j] == m[i][j-1]) c += aggregate_corners(m, corners, mark, i, j-1);
+
+  return c;
 }
 
 int main() {
@@ -79,7 +83,7 @@ int main() {
     }
   }
 
-  map<pair<int, int>, int> areas, sides;
+  map<pair<int, int>, int> areas, corners, sides;
 
   for (int i = 0; i < m.size(); i++) {
     for (int j = 0; j < m[i].size(); j++) {
@@ -90,8 +94,14 @@ int main() {
 
   for (int i = 0; i < m.size(); i++) {
     for (int j = 0; j < m[i].size(); j++) {
+      corners[make_pair(i, j)] = find_corners(m, i, j);
+    }
+  }
+
+  for (int i = 0; i < m.size(); i++) {
+    for (int j = 0; j < m[i].size(); j++) {
       if (mark2[i][j]) continue;
-      sides[make_pair(i, j)] = find_sides(m, i, j, mark2);
+      sides[make_pair(i, j)] = aggregate_corners(m, corners, mark2, i, j);
     }
   }
 
