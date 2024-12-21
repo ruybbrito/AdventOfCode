2713c92a8002633a1d8d70d1a0e27a8ff48fcc90
Ruy Brito Barbosa <bruy@fb.com>
2024-12-21 16:56:38 +0000

    Adding 21b

--- 21b.cpp
+++ 21b.cpp
@@ -9,8 +9,9 @@ using namespace std;
 // dir = 3 -> left
 
 typedef pair<int, int> pii;
+typedef pair<char, char> pcc;
 
-const int INF = 10000000;
+const long long int INF = 100000000000000000LL;
 
 char M[4][3];
 map<char, pii> MI;
@@ -110,8 +111,8 @@ string solve_second(string &s) {
 }
 
 // Count how many ways we can build s from the current state
-int solve_third(string &s) {
-  int ans = 0;
+long long int solve_third(string &s) {
+  long long int ans = 0;
   int x = 0, y = 2, cur = 0;
   while (cur < s.size()) {
     if (s[cur] == K[x][y]) ans++, cur++;
@@ -126,11 +127,11 @@ int solve_third(string &s) {
   return ans;
 }
 
-int solve(string s, int depth) {
+long long int solve(string s, int depth) {
   string ans;
   vector<string> ret = solve_first(s, ans, 0, 3, 2);
   
-  int min_len = INF;
+  long long int min_len = INF;
   for (int i = 0; i < ret.size(); i++) {
     string next = ret[i];
     for (int j = 0; j < depth; j++) {
@@ -138,6 +139,139 @@ int solve(string s, int depth) {
     }
     
     min_len = min(min_len, solve_third(next));
+  }
+
+  return min_len;
+}
+
+//     +---+---+
+//     | ^ | A |
+// +---+---+---+
+// | < | v | > |
+// +---+---+---+
+
+pair<map<pcc, long long int>, char> calculate_transitions(map<pcc, long long int> transitions, char first_character) {
+  map<pcc, long long int> next;
+  for (map<pcc, long long int>::iterator it = transitions.begin(); it != transitions.end(); it++) {
+    pcc m = it->first;
+    string trans;
+    if (m.first == m.second) {
+      next[make_pair('A','A')] += it->second;
+    } else {
+      if (m.first == '^') {
+        if (m.second == '<') {
+          trans = "Av<A";
+        } else if (m.second == '>') {
+          trans = "Av>A";
+        } else if (m.second == 'v') {
+          continue;
+        } else if (m.second == 'A') {
+          trans = "A>A";
+        }
+      } else if (m.first == '<') {
+        if (m.second == '^') {
+          trans = "A^>A";
+        } else if (m.second == '>') {
+          continue;
+        } else if (m.second == 'v') {
+          trans = "A>A";
+        } else if (m.second == 'A') {
+          trans = "A^>>A";
+        }
+      } else if (m.first == '>') {
+        if (m.second == '^') {
+          trans = "A^<A";
+        } else if (m.second == '<') {
+          continue;
+        } else if (m.second == 'v') {
+          trans = "A<A";
+        } else if (m.second == 'A') {
+          trans = "A^A";
+        }
+      } else if (m.first == 'v') {
+        if (m.second == '^') {
+          continue;
+        } else if (m.second == '<') {
+          trans = "A<A";
+        } else if (m.second == '>') {
+          trans = "A>A";
+        } else if (m.second == 'A') {
+          trans = "A^>A";
+        }
+      } else if (m.first == 'A') {
+        if (m.second == '^') {
+          trans = "A<A";
+        } else if (m.second == '<') {
+          trans = "Av<<A";
+        } else if (m.second == '>') {
+          trans = "AvA";
+        } else if (m.second == 'v') {
+          trans = "Av<A";
+        }
+      }
+
+      for (int i = 1; i < trans.size(); i++) {
+        next[make_pair(trans[i-1],trans[i])] += it->second;
+      }
+    }
+  }
+
+  // Add the extra path from A to first_character
+  string trans = "";
+  switch (first_character) {
+    case '^':
+      trans = "<A";
+      break;
+    case '<':
+      trans = "v<<A";
+      break;
+    case '>':
+      trans = "vA";
+      break;
+    case 'v':
+      trans = "v<A";
+      break;
+    case 'A':
+      trans = "A";
+      break;
+  }
+
+  for (int i = 1; i < trans.size(); i++) {
+    next[make_pair(trans[i-1],trans[i])]++;
+  }
+
+  return make_pair(next, trans[0]);
+}
+
+pair<map<pcc, long long int>, char> initial_state(string &s) {
+  map<pcc, long long int> next;
+  for (int i = 1; i < s.size(); i++) {
+    next[make_pair(s[i-1],s[i])]++;
+  }
+  
+  return make_pair(next, s[0]);
+}
+
+long long int calculate_len(map<pcc, long long int> &transitions) {
+  long long int ans = 1LL;
+  for (map<pcc, long long int>::iterator it = transitions.begin(); it != transitions.end(); it++) {
+    ans += it->second;
+  }
+  return ans;
+}
+
+long long int solve2(string s, int depth) {
+  string ans;
+  vector<string> ret = solve_first(s, ans, 0, 3, 2);
+  
+  long long int min_len = INF;
+  for (int i = 0; i < ret.size(); i++) {
+    pair<map<pcc, long long int>, char> state = initial_state(ret[i]);
+    for (int j = 0; j <= depth; j++) {
+      state = calculate_transitions(state.first, state.second);
+    }
+    
+    min_len = min(min_len, calculate_len(state.first));
   }
 
   return min_len;
@@ -195,10 +329,16 @@ void set_keypad_1() {
   }
 }
 
-int get_num(string s) {
+long long int get_num(string s) {
   s.pop_back();
-  return stoi(s);
+  return stoll(s);
 }
+
+void print_transitions(map<pcc, long long int> &transitions) {
+  for (map<pcc, long long int>::iterator it = transitions.begin(); it != transitions.end(); it++) {
+    cout << "(" << it->first.first << "," << it->first.second << ") " << it->second << endl;
+  }
+} 
 
 int main() {
   // Set keypads
@@ -206,62 +346,15 @@ int main() {
   set_keypad_1();
 
   string s;
-  int ans = 0;
+  long long int ans = 0;
   while (cin >> s) {
-    ans += (solve(s, 12) * get_num(s));
+    ans += (solve2(s, 24) * get_num(s));
+    cout << solve2(s, 24) << " " << get_num(s) << endl;
   }
   cout << ans << endl;
-  
+
   return 0; 
 }
 
 
-//     +---+---+
-//     | ^ | A |
-// +---+---+---+
-// | < | v | > |
-// +---+---+---+
-
-// ^^<A^>AvAvvA // 12
-// (^^) –> 1 turns (-)
-// (^<) –> 1 turns (v<A)
-// (<A) –> 1 turns (^>>A)
-// (A^) –> 2 turns (<A)
-// (^>) –> 1 turns (v>A)
-// (>A) –> 1 turns (^A)
-// (Av) –> 2 turns (v<A)
-// (vA) –> 2 turns (^>A)
-// (vv) –> 1 turns (-)
-// first –> ^
-
-// <AAv<A^>>A<Av>A^Av<A^>Av<AA^>A // 30
-// (^^) –> 
-// (^<) –> 
-// (^>) –> 3 turns (v>A)
-// (^A) –> 1 turns (>A)
-// (>A) –> 4 turns (^A)
-// (>>) –> 1 turns (-)
-// (vA) –> 
-// (vv) –> 
-// (v<) –> 3 turns (<A)
-// (v>) –> 1 turns (>A)
-// (<A) –> 5 turns (^>>A)
-// (A^) –> 
-// (Av) –> 
-// (AA) –> 
-
-// (78)
-// (^^) –> 
-// (^<) –> 
-// (^>) –> 5
-// (^A) –> 4
-// (>A) –> 12
-// (>>) –> 5
-// (vA) –> 
-// (vv) –> 
-// (v<) –> 
-// (v>) –> 3
-// (<A) –> 3
-// (A^) –> 
-// (Av) –> 
-// (AA) –> 
+// 274856120650160
