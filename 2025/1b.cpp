cbc74d4d09aea30976c83aa41510f19b081622f1
Ruy Brito <ruybrito3@gmail.com>
2025-12-01 10:15:57 +0000

    Add 1a

--- 2025/1b.cpp
+++ 2025/1b.cpp
@@ -14,8 +14,11 @@ int main() {
     int dir = s[0] == 'L' ? 0 : 1;
     s = s.substr(1, s.size()-1);
     int k = stoi(s);
+    int t = dir ? n+k : n-k;
     n = dir ? safe_mod(n+k, 100) : safe_mod(n-k, 100);
-    if (n == 0) {
+    if (k >= 100) {
+        ans += k/100;
+    } else if (t >= 100 || t <= 0) {
         ans++;
     }
   }
